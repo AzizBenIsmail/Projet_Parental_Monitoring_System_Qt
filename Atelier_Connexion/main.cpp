0788d550627d06818abf8459c7a75f00d277e5b3
aziz-art <61393700+aziz-art@users.noreply.github.com>
2021-03-31 11:10:05 +0100

    Step 1

--- Atelier_Connexion/main.cpp
+++ Atelier_Connexion/main.cpp
@@ -1,18 +1,17 @@
 #include "mainwindow.h"
-#include "connection.h"
 #include <QApplication>
 #include <QMessageBox>
-
+#include "connection.h"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    connection c;
+    Connection c;
     bool test=c.createconnect();
     MainWindow w;
+
     if(test)
     {w.show();
-        QMessageBox::information(nullptr, QObject::tr("Welcom"),
+        QMessageBox::information(nullptr, QObject::tr("database is open"),
                     QObject::tr("connection successful.\n"
                                 "Click Cancel to exit."), QMessageBox::Cancel);
 
@@ -22,6 +21,7 @@ int main(int argc, char *argv[])
                     QObject::tr("connection failed.\n"
                                 "Click Cancel to exit."), QMessageBox::Cancel);
 
-    w.show();
+
+
     return a.exec();
 }
---
+++ Atelier_Connexion/mainwindow.h
@@ -0,0 +1,28 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include "etudiant.h"
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_ajouter_clicked();
+
+    void on_pushButton_S_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    Etudiant e;
+};
+
+#endif // MAINWINDOW_H
